9c75166bfc8b3200cf76c3398a9b19f6ce5994a2
RobelRH <robel.redda28@gmail.com>
2023-01-18 23:11:44 +0300

    first commit

---
+++ server/server.js
@@ -0,0 +1,74 @@
+import express from 'express'
+import * as dotenv from 'dotenv'
+import cors from 'cors'
+import { Configuration, OpenAIApi } from 'openai'
+import axios from 'axios'
+
+dotenv.config()
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+app.get('/', async (req, res) => {
+  res.status(200).send({
+    message: 'Hello from CodeX!'
+  })
+})
+
+app.post('/', async (req, res) => {
+  try {
+    const prompt = req.body.prompt;
+
+    console.log(prompt)
+
+    const DATA = {
+      prompt: prompt
+    }
+    const HEADER = {
+      headers: {
+        'Origin': 'https://gpt3.ethio-tech.com',
+        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:108.0) Gecko/20100101 Firefox/108.0'
+      },
+    }
+
+    axios
+      .post('https://e1-server.ml:10333', DATA, HEADER)
+      .then((response) => {
+        if (response.status === 200) {
+          res.status(200).send({
+            bot: response.data.bot
+          });
+        }
+      })
+      .catch((e) => {
+        console.error(e)
+      })
+
+    // const response = await openai.createCompletion({
+    //   model: "text-davinci-003",
+    //   prompt: `${prompt}`,
+    //   temperature: 0, // Higher values means the model will take more risks.
+    //   max_tokens: 3000, // The maximum number of tokens to generate in the completion. Most models have a context length of 2048 tokens (except for the newest models, which support 4096).
+    //   top_p: 1, // alternative to sampling with temperature, called nucleus sampling
+    //   frequency_penalty: 0.5, // Number between -2.0 and 2.0. Positive values penalize new tokens based on their existing frequency in the text so far, decreasing the model's likelihood to repeat the same line verbatim.
+    //   presence_penalty: 0, // Number between -2.0 and 2.0. Positive values penalize new tokens based on whether they appear in the text so far, increasing the model's likelihood to talk about new topics.
+    // });
+
+    // res.status(200).send({
+    //   bot: response.data.choices[0].text
+    // });
+
+  } catch (error) {
+    console.error(error)
+    res.status(500).send(error || 'Something went wrong');
+  }
+})
+
+app.listen(5000, () => console.log('AI server started on http://localhost:5000'))
