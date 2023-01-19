cf78283966fced3676908d11ce55584953bef55b
Mubarek Seid <mubareksd@gmail.com>
2023-01-19 12:33:51 +0300

    update

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -95,7 +95,7 @@ function App() {
       Loader(messageDiv)
     }
 
-    const response = await fetch('http://localhost:5000/', {
+    const response = await fetch('https://my-gpt-server.onrender.com/', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
